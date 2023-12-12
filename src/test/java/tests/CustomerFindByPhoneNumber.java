686993b6db43befb43c9ba5ac9a4317aaf54f948
asinelnik <sinelnik.alek@yandex.ru>
2023-12-12 16:15:35 +0300

    Test

--- src/test/java/tests/CustomerFindByPhoneNumber.java
+++ src/test/java/tests/CustomerFindByPhoneNumber.java
@@ -1,7 +1,5 @@
 package tests;
 
-import io.qameta.allure.restassured.AllureRestAssured;
-import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 import services.BaseStep;
 import steps.SoapFindByPhone;
@@ -12,9 +10,7 @@ public class CustomerFindByPhoneNumber {
     SoapFindByPhone soapFindByPhone = new SoapFindByPhone();
 
     @Test(description = "Проверка регистрации владельца в SOAP", dataProvider = "authParamForGetToken", dataProviderClass = BaseStep.class)
-
     public void customerFindByPhoneNumber(String Login, String Password) throws JAXBException {
-        RestAssured.filters(new AllureRestAssured());
         soapFindByPhone.soapFindByPhone(Login, Password);
         System.out.println();
     }
